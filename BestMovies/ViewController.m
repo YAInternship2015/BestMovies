bec7072ae95145e2bc4e74be3f26e4bce63c3f38
Admin <admin@Mac-Admin.local>
2015-08-09 00:02:28 +0300

    Initial Commit

---
+++ BestMovies/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BestMovies
+//
+//  Created by Admin on 09.08.15.
+//  Copyright (c) 2015 Ignatenko_Alexandr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BestMovies/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BestMovies
+//
+//  Created by Admin on 09.08.15.
+//  Copyright (c) 2015 Ignatenko_Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
