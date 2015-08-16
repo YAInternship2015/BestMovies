43b450963a2e97e18c62d3e176118533d796dcbf
Admin <admin@Mac-Admin.local>
2015-08-17 00:46:55 +0300

    updated data source

--- BestMovies/Data/SANDataSource.m
+++ BestMovies/Data/SANDataSource.m
@@ -8,21 +8,34 @@
 
 #import "SANDataSource.h"
 
+@interface SANDataSource ()
+
+@property (nonatomic, strong) NSMutableArray *arrayMovies;
+
+@end
+
 @implementation SANDataSource
 
-- (NSArray *)allMovies {
-    NSMutableArray *arrayMovies = [NSMutableArray array];
-    NSString *movies[] = {
-        @"Transformers", @"Terminator", @"Bruce Lee", @"Men in Bleck",
-        @"Superman", @"Hulk", @"Riddick", @"Batman", @"X-man", @"Faster"
-    };
-    arrayMovies = [NSMutableArray new];
-    for (int i = 0; i < 10; i++) {
-        UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d.jpg",i + 1]];
-        SANMovie *movie = [[SANMovie alloc]initWithImage:image name:movies[i]];
-        [arrayMovies addObject:movie];
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.arrayMovies = [NSMutableArray array];
+        NSString *movies[] = {
+            @"Transformers", @"Terminator", @"Bruce Lee", @"Men in Bleck",
+            @"Superman", @"Hulk", @"Riddick", @"Batman", @"X-man", @"Faster"
+        };
+        self.arrayMovies = [NSMutableArray new];
+        for (int i = 0; i < 10; i++) {
+            UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%d.jpg",i + 1]];
+            SANMovie *movie = [[SANMovie alloc]initWithImage:image name:movies[i]];
+            [self.arrayMovies addObject:movie];
+        }
     }
-    return arrayMovies;
+    return self;
+}
+
+- (NSArray *)allMovies {
+    return self.arrayMovies;
 }
 
 @end
